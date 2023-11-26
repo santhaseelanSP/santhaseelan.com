c05398f6a84faa8188a5615be760ef69ea85df53
santhaseelanSP <135628796+santhaseelanSP@users.noreply.github.com>
2023-11-26 11:59:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const firebaseConfig = {
   messagingSenderId: "801585614125",
   appId: "1:801585614125:web:cf76af9379227465c057a4"
 };
-
-
 firebase.initializeApp(firebaseConfig);
 
 
@@ -16,20 +14,14 @@ firebase.initializeApp(firebaseConfig);
 let passwordDB;
 
 firebase.database().ref('password').on('value',(snap)=>{
-  console.log(snap.val());
   passwordDB = snap.val();
 
 });
 
 
 
-
 function checkPassword() {
-
-    console.log(passwordDB);
-
     var password = document.getElementById("passwordInput").value;
-    var contentDiv = document.getElementById("contentDiv");
   
     if (password == passwordDB) 
     {
@@ -62,16 +54,30 @@ let allDetails;
 
 function getDetails(callback)
 {
-  firebase.database().ref('edata').on('value',(snap)=>{
-  console.log(snap.val());
+  firebase.database().ref('edata').once('value',(snap)=>{
   allDetails = snap.val();
-    
+  
   });
+  
   setTimeout(function () {
     // Call the callback function after the delay
     callback();
-}, 3000);
+}, 1000);
 }
+
+function addDetailsToDb(key , value)
+{
+  var key = document.getElementById("key").value;
+  var value = document.getElementById("value").value;
+
+  firebase.database().ref('edata').set(allDetails+","+key+" : "+value);
+  document.getElementById("added").innerHTML = "<p>Added</p>";
+
+  setTimeout(function() { removeAdded(); }, 2000);
+
+}
+
+
 
 
 function displayDetails() 
@@ -82,24 +88,13 @@ function displayDetails()
   // Iterate through each element and list them
   for (var i = 0; i < detailList.length; i++) 
   {
-      console.log(detailList[i]);
       document.getElementById("contentDiv").innerHTML += "<h3>"+detailList[i]+"</h3>";
 
   }
 
 }
 
-function addDetailsToDb(key , value)
-{
-  var key = document.getElementById("key").value;
-  var value = document.getElementById("value").value;
 
-  firebase.database().ref('edata').set(allDetails+","+key+":"+value);
-  document.getElementById("added").innerHTML = "<p>Added</p>";
-
-  setTimeout(function() { removeAdded(); }, 2000);
-
-}
 
 function removeAdded()
 {
